e8e77431c604eafeef042abd22e5e873860ffc10
Alex Echanove <echanove.1@osu.edu>
2023-10-24 21:14:14 -0400

    Checkpoint 2

--- app/src/main/java/com/example/flex/MainActivity.java
+++ app/src/main/java/com/example/flex/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.flex;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.flex.model.UserAccount;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.Firebase;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,31 +28,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText name    = findViewById(R.id.nameText);
         final Button deleteBut = findViewById(R.id.deleteButt);
         final Button updateBut = findViewById(R.id.updateBut);
+
+
+        TabLayout tabLayout;
+
         deleteBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String usernameToDelete = "mk27";
-                DatabaseReference userReference = reference.child("users").child(usernameToDelete);
-                userReference.removeValue().addOnCompleteListener(task -> {
-                    Toast.makeText(MainActivity.this, "Deleted the user", Toast.LENGTH_SHORT).show();
+                final String usernameToDelete = name.getText().toString();
+                reference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild(usernameToDelete)) {
+                            DatabaseReference userReference = reference.child("users").child(usernameToDelete);
+                            userReference.removeValue().addOnCompleteListener(task -> {
+                                Toast.makeText(MainActivity.this, "Deleted the user", Toast.LENGTH_SHORT).show();
+
+                            });
+                        } else {
+                            Toast.makeText(MainActivity.this, "User doesn't exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
                 });
+
             }
         });
+
 
         updateBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String newEmail = "ec12@email.com";
-                String usernameToUpdate = "ec10"; // Replace with the appropriate username
+                final String usernameToUpdate = name.getText().toString();
+                reference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild(usernameToUpdate)) {
+                            String newEmail = "temp@email.com";
+                            DatabaseReference userReference = reference.child("users").child(usernameToUpdate);
+                            userReference.child("email").setValue(newEmail).addOnCompleteListener(task -> {
+                                Toast.makeText(MainActivity.this, "Updated the user", Toast.LENGTH_SHORT).show();
+                            });
+                        } else {
+                            Toast.makeText(MainActivity.this, "User doesn't exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
 
-                DatabaseReference userReference = reference.child("users").child(usernameToUpdate);
-                userReference.child("email").setValue(newEmail).addOnCompleteListener(task -> {
-                    Toast.makeText(MainActivity.this, "Updated the user", Toast.LENGTH_SHORT).show();
+                    }
                 });
+
             }
         });
-
     }
 }
